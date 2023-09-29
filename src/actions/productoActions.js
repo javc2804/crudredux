fa0730b47c35f7c1d32096f2fc22d95a88956d64
javc1995 <146193554+javc1995@users.noreply.github.com>
2023-09-29 14:01:41 -0400

    add redux

---
+++ src/actions/productoActions.js
@@ -0,0 +1,13 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types'
+
+// Crear nuevos productos
+
+export function crearNuevoProductoAction(producto) {
+    return () => {
+        console.log(producto);
+    }
+}

22ad63ff6cc6adc4bf87146625211dc7a18a0456
javc1995 <146193554+javc1995@users.noreply.github.com>
2023-09-29 11:37:12 -0400

    insert only maque

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import React from 'react'
+import Header from './components/Header'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Productos from './components/Productos'
+import NuevoProducto from './components/NuevoProducto'
+import EditarProducto from './components/EditarProducto'
+
 function App() {
   return (
-    <h1>Redux</h1>
+    <Router>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route exact path="/" element={<Productos />} />
+          <Route exact path="/productos/nuevo" element={<NuevoProducto />} />
+          <Route exact path="/productos/editar/:id" element={<EditarProducto />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
